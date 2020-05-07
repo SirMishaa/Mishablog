221494f54d4ad5c2311d6bf92addaec54db67dee
Misha <mishapro@mm.st>
2020-05-07 11:31:37 +0200

    Feat : Set up Webpack encore and Tailwindcss
    Install Webpack encore to be able to load and compile assets and install Tailwindcss with PostCSS.

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+    plugins: [
+        tailwindcss('./tailwind.config.js'), // your tailwind.js configuration file path
+        require('autoprefixer'),
+        require('postcss-import')
+    ]
+}
